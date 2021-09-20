1289d7245a07129053c56d0eedd862d1af2f677e
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-09-21 04:24:49 +0600

    something change

--- src/Components/Countries/Countries.js
+++ src/Components/Countries/Countries.js
@@ -13,7 +13,8 @@ const Countries = () => {
       <h2 className="ps-4">Travelling around the world</h2>
       <h4 className="ps-4">Available Countries: {countries.length}</h4>
       {countries.map((country) => (
-        <Country name={country.name} capital={country.capital} population={country.population}></Country>
+       
+        <Country country={country}></Country>
       ))}
     </div>
   );
--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -4,9 +4,11 @@ import "./Country.css"
 const Country = (props) => {
   return (
     <div className=" m-4 p-3 country">
-      <h2>Name:{props.name} </h2>
-      <h4>Capital: {props.capital}</h4>
-        <p>Population: {props.population}</p>
+
+      <h2>Name:{props.country.name} </h2>
+      <img title={props.country.name} className="img-fluid w-25" src={props.country.flag} alt="Country-flag"/>
+      <h4>Capital: {props.country.capital}</h4>
+        <p>Population: {props.country.population}</p>
     </div>
   );
 };
