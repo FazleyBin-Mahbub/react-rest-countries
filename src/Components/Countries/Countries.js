d0c03af7b31c958a7c0886d2adce9c6a16d2f06f
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-09-21 04:52:56 +0600

    added

--- src/Components/Countries/Countries.js
+++ src/Components/Countries/Countries.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Country from "../Country/Country";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Countries.css";
 const Countries = () => {
   const [countries, setCountries] = useState([]);
   useEffect(() => {
@@ -9,13 +10,16 @@ const Countries = () => {
       .then((data) => setCountries(data));
   }, []);
   return (
-    <div className="mt-3">
-      <h2 className="ps-4">Travelling around the world</h2>
-      <h4 className="ps-4">Available Countries: {countries.length}</h4>
-      {countries.map((country) => (
-       
-        <Country country={country}></Country>
-      ))}
+    <div>
+      <div className="heading ps-4">
+        <h2>Travelling around the world</h2>
+        <h4>Available Countries: {countries.length}</h4>
+      </div>
+      <div className="countries">
+        {countries.map((country) => (
+          <Country country={country}></Country>
+        ))}
+      </div>
     </div>
   );
 };
--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -1,14 +1,15 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css"
-import "./Country.css"
-const Country = (props) => {
-  return (
-    <div className=" m-4 p-3 country">
 
-      <h2>Name:{props.country.name} </h2>
-      <img title={props.country.name} className="img-fluid w-25" src={props.country.flag} alt="Country-flag"/>
-      <h4>Capital: {props.country.capital}</h4>
-        <p>Population: {props.country.population}</p>
+import "./Country.css";
+const Country = (props) => {
+  const {name,flag,region,capital,population}=props.country
+  return (
+    <div className="country">
+      <h1>Name: {props.country.name}</h1>
+      <img className="country-flag" title={name} src={flag} alt="" />
+      <p>Region: {region}</p>
+      <h4>Capital: {capital}</h4>
+      <h5>Population: {population}</h5>
     </div>
   );
 };
